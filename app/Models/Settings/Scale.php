18a13653e55d68093816547da3f3b097e3532642
Lloyd Culpepper <lloydculpepper4@gmail.com>
2020-12-15 21:25:01 +0000

    added settings namespace & background setting

--- app/Models/Settings/Scale.php
+++ app/Models/Settings/Scale.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Settings;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ScaleSettings extends Model
+class Scale extends Model
 {
     use HasFactory;
 
