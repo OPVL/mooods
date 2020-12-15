d50223c6a9cc45c142e20a818be9df8aa3b9c9c2
Lloyd Culpepper <lloydculpepper4@gmail.com>
2020-12-15 01:00:24 +0000

    moods controllers uipdsated

--- app/Http/Controllers/MoodsController.php
+++ app/Http/Controllers/MoodsController.php
@@ -2,26 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Mood\CreateMood;
+use App\Http\Requests\StoreMood;
 use App\Models\Mood;
+use App\Models\ScaleSettings;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class MoodsController extends Controller
 {
-    public function index(): Response
+    public function index(): RedirectResponse
     {
-        return Inertia::render('Mood/Create');
+        // disable old route
+        return redirect()->route('dashboard');
     }
 
-    public function create(): Response
+    public function create(): View
     {
-        return Inertia::render('Mood/Create', [
-            'test' => 'Fuck off cunt',
-        ]);
+        $scalePrefs = (new ScaleSettings([
+            'type' => 'dots',
+            'levels' => 7,
+        ]))->toArray();
+
+        $this->data = [
+            'scale' => $scalePrefs, // user prefs
+            'question' => 'ur a heddgehog, no?', // user prefs
+            'route' => route('mood.store'),
+        ];
+
+        return view('pages.moods.create', $this->data);
+    }
+
+    public function store(StoreMood $request, CreateMood $action): RedirectResponse {
+        $result = $action->execute($request->validated());
+
+        if ($result) {
+            return redirect()->route('mood.index');
+        }
+
+        return redirect()->back();
     }
 
     public function edit(Mood $mood): Response
     {
         return Inertia::render('Mood/Edit', compact('mood'));
     }
+
+    public function update(Mood $mood) {}
+
+    public function destroy(Mood $mood) {}
 }
---
+++ app/Http/Controllers/ThoughtsController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ThoughtsController extends Controller
+{
+    
+}
