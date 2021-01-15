cae7d487227c8c3fd208c8e6c68eec6086f4328f
Lloyd Culpepper <lloydculpepper4@gmail.com>
2021-01-15 23:46:49 +0000

    recent scope added to mood

--- app/Models/Mood.php
+++ app/Models/Mood.php
@@ -2,8 +2,12 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Mood extends Model
 {
@@ -12,9 +16,24 @@ class Mood extends Model
     protected $fillable = [
         'value'
     ];
-    
-    public function thoughts()
+
+    public function thoughts(): HasMany
     {
         return $this->hasMany(Thought::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeRecent(Builder $query): Builder
+    {
+        return $query
+            ->where(
+                'created_at',
+                '>',
+                Carbon::now()->subHours(config('moood.recent', 3))
+            );
     }
 }
